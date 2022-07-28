b45ab35da105e1e573e13eb148214b97451856be
sla6ak <slabakxaker@gmail.com>
2022-07-28 07:17:41 +0300

    настройка бекенда

--- src/redux/testURL.js
+++ src/redux/testURL.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://localhost:5000';
-// const BASE_URL = ''
+// const BASE_URL = 'https://localhost:5000';
+const BASE_URL = 'https://git.heroku.com/api-team-wallet.git';
 
 export default BASE_URL;
