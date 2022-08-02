e61e1cfc514a66951cb7404a5eef26a45802bc36
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2022-08-02 22:52:14 +0100

    currency without statuses

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,27 +1,48 @@
+import { useEffect, useState } from 'react';
 import {
     Container,
     Header,
     Table,
     ListCurrency,
+    ItemCurrency,
 } from './Currency.styled';
 
 const Currency = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+        .then(response => response.json())
+        .then(response => setData(response))
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
+
     return (
-        <Container>
-            <Header>
-                <li>Currency</li>
-                <li>Purchase</li>
-                <li>Sale</li>
-            </Header>
-            <Table>
-                <ListCurrency>
-                    <li><span>1</span></li>
-                    <li><span>2</span></li>
-                    <li><span>3</span></li>
-                </ListCurrency>
-            </Table>
-        </Container>
-    )
+        <>
+        {data && (
+            <Container>
+                <Header>
+                    <li>Currency</li>
+                    <li>Purchase</li>
+                    <li>Sale</li>
+                </Header>
+                <Table>
+                    <ListCurrency>
+                    {data.map(item => (
+                        <ItemCurrency key={item.ccy}>
+                            <span>{item.ccy}</span>
+                            <span>{Number(item.buy)}</span>
+                            <span>{Number(item.sale)}</span>
+                        </ItemCurrency>
+                    ))}
+                    </ListCurrency>
+                </Table>
+            </Container>
+        )}
+        </>
+    );
 }
 
 export default Currency;
