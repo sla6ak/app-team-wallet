6ed2c449fe9aea7d8b22ae73febf138a98cbee7c
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2022-08-04 23:18:34 +0100

    modal logOut

---
+++ src/components/modalLogOut/ModalLogOut.styled.jsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const LogOutContainer = styled.div`
+    width: 410px;
+`;
+
+export const ModalTitle = styled.h3`
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 36px;
+    text-align: center;
+    color: #000000;
+    @media (min-width: 768px) {
+        font-size: 24px;
+        line-height: 45px;
+    }
+`;
+
+export const TitleAccent = styled.p`
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 36px;
+`;
+
+export const ButtonWrapper = styled.div`
+    width: 350px;
+    margin-top: 42px;
+    display: flex;
+
+    margin-left: auto;
+    margin-right: auto;
+`;
