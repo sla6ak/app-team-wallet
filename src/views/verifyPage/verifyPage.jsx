591d070bb67f85a788de52ddbe0a9cef59935889
Volodymyr Horbatiuk <vladimir.a.gorbatyuk@gmail.com>
2022-08-09 20:10:00 +0300

    clean comments/trash

---
+++ src/views/verifyPage/verifyPage.jsx
@@ -0,0 +1,25 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { fetch } from 'components/verifyEmail/fetch';
+import { toastSuccess, toastErr } from 'components/verifyEmail/toast';
+
+export const VerifyPage = () => {
+    const { verificationToken } = useParams();
+
+    fetch(verificationToken)
+        .then(res => {
+            const status = res.status;
+            console.log(status, 'then');
+            toastSuccess();
+        })
+        .catch(error => {
+            const status = error.response.status;
+            console.log(status, 'catch');
+            toastErr();
+        });
+
+    return (
+        <div>
+            <Navigate to="/login" />
+        </div>
+    );
+};
