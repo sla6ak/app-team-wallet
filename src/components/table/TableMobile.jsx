41a02df9466ec19e0a2ff66f48471bec0d4397f4
Mikhail-Ropii <mr.ropiy@gmail.com>
2022-08-10 18:52:08 +0300

    Update TableMobile.jsx

--- src/components/table/TableMobile.jsx
+++ src/components/table/TableMobile.jsx
@@ -8,65 +8,67 @@ const TableMobile = ({ allTransactions }) => {
         return String(value).padStart(2, '0');
     }
     return (
-        <TabWraper>
+        <>
             <Media query="(max-width: 767px)" render={() => <Balance />} />
-            {allTransactions.transactions
-                .slice()
-                .reverse()
-                .map(data => (
-                    <TableContainer
-                        key={data._id}
-                        sx={TableStyled}
-                        style={{ borderColor: data.type === 'income' ? '#24CCA7' : '#FF6596' }}
-                    >
-                        <Table>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCellStyled align="left">Date</TableCellStyled>
-                                    <TableCellValueStyled align="right">
-                                        {addZero(data.date.day)}.{addZero(data.date.month)}.{data.date.year}
-                                    </TableCellValueStyled>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCellStyled align="left">Type</TableCellStyled>
-                                    <TableCellValueStyled align="right">{data.type}</TableCellValueStyled>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCellStyled align="left">Category</TableCellStyled>
-                                    <TableCellValueStyled align="right">{data.category}</TableCellValueStyled>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCellStyled align="left">Comment</TableCellStyled>
-                                    <TableCellValueStyled
-                                        style={{ maxWidth: '150px', overflow: 'hidden', textOverflow: 'ellipsis' }}
-                                        align="right"
-                                    >
-                                        {data.comment}
-                                    </TableCellValueStyled>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCellStyled align="left">Sum</TableCellStyled>
-                                    <TableCellValueStyled
-                                        style={{
-                                            fontWeight: 700,
-                                            color: data.type === 'income' ? '#24CCA7' : '#FF6596',
-                                        }}
-                                        align="right"
-                                    >
-                                        {data.sum}
-                                    </TableCellValueStyled>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCellStyled align="left">Balance</TableCellStyled>
-                                    <TableCellValueStyled align="right">
-                                        {data.balanceAfterTransaction}
-                                    </TableCellValueStyled>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                ))}
-        </TabWraper>
+            <TabWraper>
+                {allTransactions.transactions
+                    .slice()
+                    .reverse()
+                    .map(data => (
+                        <TableContainer
+                            key={data._id}
+                            sx={TableStyled}
+                            style={{ borderColor: data.type === 'income' ? '#24CCA7' : '#FF6596' }}
+                        >
+                            <Table>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCellStyled align="left">Date</TableCellStyled>
+                                        <TableCellValueStyled align="right">
+                                            {addZero(data.date.day)}.{addZero(data.date.month)}.{data.date.year}
+                                        </TableCellValueStyled>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCellStyled align="left">Type</TableCellStyled>
+                                        <TableCellValueStyled align="right">{data.type}</TableCellValueStyled>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCellStyled align="left">Category</TableCellStyled>
+                                        <TableCellValueStyled align="right">{data.category}</TableCellValueStyled>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCellStyled align="left">Comment</TableCellStyled>
+                                        <TableCellValueStyled
+                                            style={{ maxWidth: '150px', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                                            align="right"
+                                        >
+                                            {data.comment}
+                                        </TableCellValueStyled>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCellStyled align="left">Sum</TableCellStyled>
+                                        <TableCellValueStyled
+                                            style={{
+                                                fontWeight: 700,
+                                                color: data.type === 'income' ? '#24CCA7' : '#FF6596',
+                                            }}
+                                            align="right"
+                                        >
+                                            {data.sum}
+                                        </TableCellValueStyled>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCellStyled align="left">Balance</TableCellStyled>
+                                        <TableCellValueStyled align="right">
+                                            {data.balanceAfterTransaction}
+                                        </TableCellValueStyled>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    ))}
+            </TabWraper>
+        </>
     );
 };
 
